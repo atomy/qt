efd4dbb3774360a904085577aeaf31a388c736f1
atomy <atomy@jackinpoint.net>
2013-04-08 23:19:46 +0200

    - Arc-Borders are now being rendered

--- goaler/GoalRenderer.cpp
+++ goaler/GoalRenderer.cpp
@@ -3,11 +3,14 @@
 #include "GoalRenderer.h"
 #include <iostream>
 #include <math.h>
+#include <QTime>
 
 #define CIRCLE_RADIUS_PER_LEVEL 100
 
 GoalRenderer::GoalRenderer()
 {
+    // Get random value
+    qsrand(QTime::currentTime().msec());
 }
 
 void GoalRenderer::setEvent(QPaintEvent* event)
@@ -24,100 +27,56 @@ void GoalRenderer::render(GoalElement* ele)
 }
 
 void GoalRenderer::_drawElement(QPainter &painter, GoalElement* ele)
-{
-    float rectX, rectY, rectW, rectH;
+{   
+    std::cout << "draing lvl: " << ele->getLevel()
+              << " startangle: " << ele->getStartAngle()
+              << " endangle: " << ele->getEndAngle() << std::endl;
+
     int w = m_pWidget->width();
     int h = m_pWidget->height();
-    int m = (w > h ? h : w);
-    int level = ele->getLevel();
 
-    rectX = (m/2) - CIRCLE_RADIUS_PER_LEVEL * level;
-    rectY = (m/2) - CIRCLE_RADIUS_PER_LEVEL * level;
-    rectW = CIRCLE_RADIUS_PER_LEVEL * level * 2;
-    rectH = CIRCLE_RADIUS_PER_LEVEL * level * 2;
+    mRectSize = (w > h ? h : w);
+    mRadius = CIRCLE_RADIUS_PER_LEVEL * ele->getLevel();
+    mRectX = (mRectSize/2) - mRadius;
+    mRectY = (mRectSize/2) - mRadius;
+    mRectW = mRadius * 2;
+    mRectH = mRadius * 2;
 
+    _drawArc(painter, ele);
+    _drawArcBorders(painter, ele);
+}
+
+void GoalRenderer::_drawArcBorders(QPainter &painter, GoalElement* ele)
+{
+    if (ele->getLevel() > 1) {
+        float x1, x2, y1, y2;
+        getCoordsForCircle(mRadius, ele->getStartAngle(), x1, y1);
+        getCoordsForCircle(mRadius - CIRCLE_RADIUS_PER_LEVEL, ele->getStartAngle(), x2, y2);
+
+        x1 += mRectSize/2;
+        y1 += mRectSize/2;
+        x2 += mRectSize/2;
+        y2 += mRectSize/2;
+
+        painter.setPen(QPen(QColor(qrand() % 255, qrand() % 255, qrand() % 255), 3));
+        painter.drawLine(QPointF(x1, y1), QPointF(x2, y2));
+    }
+}
+
+void GoalRenderer::_drawArc(QPainter &painter, GoalElement* ele)
+{
     // arc
     painter.setPen(QPen(QColor(255, 255, 240), 3));
-    QRectF rect(rectX, rectY, rectW, rectH);
+    QRectF rect(mRectX, mRectY, mRectW, mRectH);
     painter.drawRect(rect); // debug
     painter.setPen(QPen(QColor(255, 0, 0), 3));
     painter.drawArc(rect, 16 * ele->getStartAngle(), 16 * ele->getEndAngle());
 }
 
-void GoalRenderer::_drawCircleForLevel(QPainter &painter, int level)
+void GoalRenderer::getCoordsForCircle(float radius, float angle, float& x, float& y)
 {
-    float rectX, rectY, rectW, rectH;
-    int w = m_pWidget->width();
-    int h = m_pWidget->height();
-
-    rectX = (w/2) - CIRCLE_RADIUS_PER_LEVEL * level;
-    rectY = (h/2) - CIRCLE_RADIUS_PER_LEVEL * level;
-    rectW = CIRCLE_RADIUS_PER_LEVEL * level * 2;
-    rectH = CIRCLE_RADIUS_PER_LEVEL * level * 2;
-
-    // arc
-    painter.setPen(QPen(QColor(255, 255, 240), 3));
-    QRectF rect(rectX, rectY, rectW, rectH);
-    painter.drawRect(rect);
-    painter.setPen(QPen(QColor(255, 0, 0), 3));
-    painter.drawEllipse(rect);
-}
-
-void GoalRenderer::renderOld(GoalElement* ele)
-{
-    QPainter painter(m_pWidget);
-	painter.setRenderHint(QPainter::Antialiasing, true);
-
-        painter.setPen(QPen(QColor(255, 0, 0), 3));
-        painter.resetTransform();
-
-        //painter.translate(50, 100);
-
-        float x1, y1, x2, y2;
-
-        // arc
-        painter.setPen(QPen(QColor(128, 0, 128), 3));
-        QRectF rect1(0.0, 0.0, 800.0, 800.0);
-        painter.drawRect(rect1);
-        painter.setPen(QPen(QColor(255, 0, 0), 3));
-        painter.drawEllipse(rect1);
-
-        // arc
-        painter.setPen(QPen(QColor(0, 255, 0), 3));
-        QRectF rect2(200.0, 200.0, 400.0, 400.0);
-        int startAngle2 = 30 * 16;
-        int spanAngle2 = 120 * 16;
-        painter.drawEllipse(rect2);
-
-        // line        
-        getCoordsForCircle(400.0, 400.0, 45.0, x1, y1);
-        getCoordsForCircle(400.0, 200.0, 45.0, x2, y2);
-        painter.setPen(QPen(QColor(0, 0, 255), 3));
-        painter.drawLine(x1, y1, x2, y2);
-
-        // line        
-        getCoordsForCircle(400.0, 400.0, 135.0, x1, y1);
-        getCoordsForCircle(400.0, 200.0, 135.0, x2, y2);
-        painter.setPen(QPen(QColor(128, 128, 0), 3));
-        painter.drawLine(x1, y1, x2, y2);
-
-        // line
-        getCoordsForCircle(400.0, 400.0, 235.0, x1, y1);
-        getCoordsForCircle(400.0, 200.0, 235.0, x2, y2);
-        painter.setPen(QPen(QColor(255, 0, 0), 3));
-        painter.drawLine(x1, y1, x2, y2);
-
-        // line
-        getCoordsForCircle(400.0, 400.0, 315.0, x1, y1);
-        getCoordsForCircle(400.0, 200.0, 315.0, x2, y2);
-        painter.setPen(QPen(QColor(255, 255, 0), 3));
-        painter.drawLine(x1, y1, x2, y2);
-}
-
-void GoalRenderer::getCoordsForCircle(float center, float radius, float angle, float& x, float& y)
-{
-    x = center + radius * std::cos(2 * M_PI * (angle / 360.0));
-    y = center + radius * std::sin(2 * M_PI * (angle / 360.0));
+    x = radius * std::cos(2 * M_PI * (angle / 360.0));
+    y = radius * std::sin(2 * M_PI * (angle / 360.0));
 }
 
 void GoalRenderer::setWidget(QWidget* widget)
--- goaler/GoalRenderer.h
+++ goaler/GoalRenderer.h
@@ -15,14 +15,17 @@ public:
     void renderOld(GoalElement* ele);
 	void setEvent(QPaintEvent*);
 	void setWidget(QWidget* widget);
-    void getCoordsForCircle(float center, float radius, float angle, float& x, float& y);
+    void getCoordsForCircle(float radius, float angle, float& x, float& y);
 
 private:
-    void _drawCircleForLevel(QPainter &painter, int);
     void _drawElement(QPainter &painter, GoalElement* ele);
+    void _drawArc(QPainter &painter, GoalElement* ele);
+    void _drawArcBorders(QPainter &painter, GoalElement* ele);
 
 	QPaintEvent* m_pEvent;
 	QWidget* m_pWidget;
+    float mRadius, mRectX, mRectY, mRectH, mRectW;
+    int mRectSize;
 };
 
 #endif // GOALRENDERER_H
